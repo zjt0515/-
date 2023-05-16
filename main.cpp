4597efc7f6a92bd0a5a1fefb06730ee029ea77dd
zjt0515 <133624596+zjt0515@users.noreply.github.com>
2023-05-16 22:52:44 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,13 +3,14 @@
 #include <cstdio>
 #include <easyx.h>
 #include <cstdlib>
-#define ROW 5				//地图行数
-#define COL 5				//地图列数
-#define LANDMINE_NUM 2	//地雷数量
+#define ROW 10			//地图行数
+#define COL 10			//地图列数
+#define LANDMINE_NUM 10	//地雷数量
+#define LENGTH 40		//图片长度，即每个格子的长度
+#define WIDTH 40		//图片宽度，即每个格子的宽度
 using namespace std;
 
 double TimeStart, TimeEnd;
-
 int difficulty = 1; //难度默认1，一共1，2，3三个难度
 
 int NearBy[ROW + 2][COL + 2];	//二维数组每个位置记录周围8个方块地雷数量(-1是地雷)
@@ -17,6 +18,9 @@ int NearBy[ROW + 2][COL + 2];	//二维数组每个位置记录周围8个方块地雷数量(-1是地雷
 //细节：数组长宽比地图行列数都多了2，也就是数组范围从0~row+1和0~col+1，但实际使用范围只用到1~row和1~col，前后多出来的1位防止后面计算NearBy时的数组越位问题(详细说就是计算时每次遇到地雷，都要将地雷周围8个格子对应的数字++，那么如果地雷在边角，地雷周围8个格子就不全在地图内了，这时如果不扩大数组，数组就会越位，强行防止越位需要写一大堆判断语句)
 
 char Map[ROW + 2][COL + 2];//显示给用户的地图
+IMAGE Img[20];//保存图片
+int cnt = 0;
+
 
 //未实现（写着玩的）
 struct user
@@ -30,15 +34,16 @@ struct user
 //函数声明
 void Menu();//菜单
 void LoadMap();//加载地图
-void PrintNearBy();//打印
-void PrintMap();//打印
-void Input();//输入
-void Islandmine(int flag, int x, int y);//输入后数据处理
+void MouseClick();//点击
+void Islandmine(int x, int y);//输入后数据处理
 void Judge(int x, int y);//胜利或失败判断
 void open(int x, int y);//打开0后炸开一片的递归
-void GameOver();		//游戏结束！ 
 void Login();			//登录
 void StartGame();		//开始游戏
+void Show();
+void LoadImages();
+void Mark(int x,int y);
+void PrintNearBy();//打印下面的数字，测试专用
 
 
 //加载地图，包括随机埋地雷，并初始化NearBy，初始化map
@@ -80,8 +85,7 @@ void LoadMap()
 	}
 	//初始化地图map
 	memset(Map, '*', sizeof(Map));
-	PrintMap();
-
+	Show();
 }
 
 //打印NearBy
@@ -107,69 +111,54 @@ void PrintNearBy()
 	cout << "---------------------" << endl;
 }
 
-//打印Map
-void PrintMap()
-{
-	system("cls");
-	for (int k = 0; k <= COL; k++)
-	{
-		printf("%3d", k);
-	}
-	cout << endl;
-	cout << "---------------------" << endl;
-	for (int i = 1; i <= ROW; i++)
-	{
-		printf("%d |", i);
-		for (int j = 1; j <= COL; j++)
-		{
-			printf("%3c", Map[i][j]);
-		}
-		cout << endl;
-	}
-	cout << "---------------------" << endl;
-}
-
-
 
 //判定是否是地雷，是否不是地雷
-void Islandmine(int flag, int x, int y)
+void Islandmine(int x, int y)
 {
-	//模拟鼠标右键标记
-	if (flag == 1)
+	//遇到已经打开的或者已经标记了的
+	if (Map[x][y]=='@'||Map[x][y]!='*')
 	{
-		if (Map[x][y] == '@')
-		{
-			Map[x][y] = '*';
-			cout << "取消标记成功" << endl;
-			PrintMap();
-		}
-		else
-		{
-			Map[x][y] = '@';
-			cout << "标记成功" << endl;
-			PrintMap();
-		}
+		return;
 	}
-	//模拟鼠标左键打开
+	//是地雷但是第一次打开
+	else if (NearBy[x][y] == -1 && cnt ==0)
+	{
+		
+	}
+	else if (NearBy[x][y] == -1)
+	{
+		Map[x][y] = NearBy[x][y] + 48;//先打开
+		Show();
+		PrintNearBy();
+		Judge(x, y);
+	}
+	//不是地雷
 	else
 	{
-		//遇到地雷
-		if (NearBy[x][y] == -1)
-		{
-			Judge(x, y);
-		}
-
-		//不是地雷
-		else
-		{
-			Map[x][y] = NearBy[x][y] + 48;//先打开
-			open(x, y);//如果是0，就要特殊操作了
-			PrintMap();
-			PrintNearBy();
-			Judge(x, y);//每次输入后都要判断是否胜利
-		}
+		Map[x][y] = NearBy[x][y] + 48;//先打开
+		open(x, y);//如果是0，就要特殊操作了
+		Show();
+		PrintNearBy();
+		Judge(x, y);//每次输入后都要判断是否胜利
 	}
 }
+
+void Mark(int x,int y)
+{
+	if (Map[x][y] == '@')
+	{
+		Map[x][y] = '*';
+		cout << "取消标记成功" << endl;
+		Show();
+	}
+	else
+	{
+		Map[x][y] = '@';
+		cout << "标记成功" << endl;
+		Show();
+	}
+}
+
 //递归点开格子
 void open(int x, int y)
 {
@@ -187,6 +176,7 @@ void open(int x, int y)
 				
 					if (Map[i][j]=='*' && (i != x || j != y))//没有被打开，并且不是自己
 					{
+						cnt++;
 						Map[i][j] = NearBy[i][j] + 48;//0旁边8个一定会被打开
 						open(i, j);//递归调用旁边8个，旁边8个如果是0就和刚才一样，不是0,open的判断就过不去，不会再递归调用，仅仅打开了自己
 					}
@@ -196,25 +186,21 @@ void open(int x, int y)
 	}
 }
 
-//用户输入
-void Input()
+//用户点击
+void MouseClick()
 {
-	int flag, x, y;
-	cout << "请输入: 1.标记地雷 0.标记不是地雷，再输入坐标 ,格式：0 1 2" << endl;
-	scanf_s("%d%d%d", &flag, &x, &y);
+	ExMessage msg;
+	if (peekmessage(&msg))
+	{
+		int x = msg.y / WIDTH + 1;
+		int y = msg.x / LENGTH + 1;
+		switch (msg.message)
+		{
+		case WM_LBUTTONDOWN: Islandmine(x, y); break;
+		case WM_RBUTTONDOWN: Mark(x,y); break;
+		}
+	}
 
-	//判断坐标是否输入正确
-	if (x >= 1 && x <= ROW && y >= 1 && y <= COL && (Map[x][y] == '*'||Map[x][y] =='@'))
-	{
-		cout << "输入正确" << endl;
-		Islandmine(flag, x, y);
-	}
-	//坐标输入错误
-	else
-	{
-		cout << "坐标错误，可能是越过地图了或者您已经点开了该点" << endl;
-		Input();
-	}
 }
 
 //胜利或失败判断，每输入一次后判断一下
@@ -224,7 +210,16 @@ void Judge(int x, int y)
 	int Count = 0;
 	if (NearBy[x][y] == -1)
 	{
-		GameOver();
+		HWND choice = GetHWnd();
+		int ch = MessageBox(choice, "蹦蹦炸弹！", "失败", MB_OKCANCEL);
+		if (ch == IDOK)
+		{
+			StartGame();
+		}
+		else
+		{
+			closegraph();
+		}
 	}
 	else
 	{
@@ -242,17 +237,15 @@ void Judge(int x, int y)
 	cout << Count<<endl;
 	if (Count == LANDMINE_NUM)
 	{
-		cout << "获胜，耗时:";
-		cout << "是否再来一把？y/n" << endl;
-		char ch;
-		cin >> ch;
-		if (ch == 'y')
+		HWND choice = GetHWnd();
+		int ch = MessageBox(choice, "您获胜了，耗时：是否在玩一次", "很好", MB_OKCANCEL);
+		if (ch == IDOK)
 		{
 			StartGame();
 		}
 		else
 		{
-			exit(0);
+			closegraph();
 		}
 	}
 }
@@ -261,7 +254,10 @@ void Judge(int x, int y)
 
 int main()
 {
+	initgraph(LENGTH * COL, WIDTH * ROW,EX_SHOWCONSOLE);
+	LoadImages();
 	Menu();
+	closegraph();
 	return 0;
 }
 
@@ -285,29 +281,13 @@ void Menu()
 		break;
 	}
 }
-//游戏失败
-void GameOver()
-{
-	cout << "踩到地雷，游戏失败" << endl;
-	PrintNearBy();
-	cout << "是否再来一把？y/n" << endl;
-	char ch;
-	if ((ch = getchar()) == 'y')
-	{
-		StartGame();
-	}
-	else
-	{
-		exit(0);
-	}
-}
 
 //开始游戏
 void StartGame()
 {
 	LoadMap();
 	while (true) {
-		Input();
+		MouseClick();
 	}
 	
 }
@@ -327,4 +307,55 @@ void Login()
 //排行榜相关(未完成)
 void rank() {
 
+}
+
+//根据map中的数字/符号显示对应图片
+void Show()
+{
+	for (int i = 1; i <= ROW; i++)
+	{
+		for (int j = 1; j <= COL; j++)
+		{
+			//如果没有被翻开就放置图片10
+			if (Map[i][j] == '*')
+			{
+				putimage(j * 40-40, i * 40-40, &Img[10]);
+			}
+			//如果数字被翻开了就显示对应图片
+			else if (Map[i][j] >= 48 && Map[i][j] <= 56)
+			{
+				for (int k = 0; k < 9; k++)
+				{
+					if (Map[i][j] == k + 48)
+					{
+						putimage(j * 40 - 40, i * 40 - 40, &Img[k]);
+					}
+				}
+				
+			}
+			//如果翻开了地雷就显示地雷图片
+			else if (Map[i][j] != '*' && Map[i][j] != '@' && NearBy[i][j] == -1)
+			{
+				putimage(j * 40 - 40, i * 40 - 40, &Img[9]);
+			}
+			//如果被标记了就显示标记图片
+			else if (Map[i][j]=='@')
+			{
+				putimage(j * 40 - 40, i * 40 - 40, &Img[11]);
+			}
+		}
+	}
+}
+
+//加载图片
+void LoadImages()
+{
+	HWND hnd = GetHWnd();
+	SetWindowText(hnd, "不会玩扫雷");
+	char path[50];
+	for (int i = 0; i < 12; i++)
+	{
+		sprintf(path, "./images/%d.jpg", i);
+		loadimage(&Img[i], path,LENGTH,WIDTH);
+	}
 }
